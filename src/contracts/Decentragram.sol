effbe6466c40737961c59155a420a47c4b364807
GauravKumarGR8 <bsn.gauravkr1@gmail.com>
2021-09-03 22:06:20 +0530

    added the complete decentragram project

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,97 @@
 pragma solidity ^0.5.0;
 
 contract Decentragram {
-  // Code goes here...
+  string public name = "Decentragram";
+
+  // Store Images
+  uint public imageCount = 0;
+  mapping (uint => Images) public images;
+
+  struct Images {
+    uint id;
+    string hash;
+    string description;
+    uint tipAmount;
+    address payable author;
+  }
+
+  event imageCreated(
+    uint id,
+    string hash,
+    string description,
+    uint tipAmount,
+    address payable author
+  );
+
+  event imageTipped(
+    uint id,
+    string hash,
+    string description,
+    uint tipAmount,
+    address payable author
+  );
+
+  // Create Images
+  function uploadImage(string memory _imgHash, string memory _description) public {
+    // Make sure the image hash exists
+    require(bytes(_imgHash).length > 0);
+
+    // Make sure image description exists
+    require(bytes(_description).length > 0);
+
+    // Make sure uploader address exists
+    require(msg.sender != address(0x0));
+
+    // Increment Image id
+    imageCount++ ;
+
+    // Add Image to contract
+    images[imageCount] = Images(imageCount, _imgHash, _description, 0, msg.sender);
+    
+    // Trigger an Event
+    emit imageCreated(imageCount, _imgHash, _description, 0, msg.sender);
+  }
+
+  // Tip Images
+  function tipImageOwner(uint _id) public payable{
+    // Make sure the id is valid
+    require(_id > 0 && _id <= imageCount);
+
+    // Fetch the Image
+    Images memory _image = images[_id];
+
+    // Fetch the author
+    address payable _author = _image.author;
+
+    // Pay the author by sending them ether
+    address(_author).transfer(msg.value);
+
+    // Increment the tip Amount
+    _image.tipAmount += msg.value;
+
+    // Update the image
+    images[_id] = _image;
+
+    // Trigger an Event
+    emit imageTipped(_id, _image.hash, _image.description, _image.tipAmount, _author);
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test/test.js
+++ test/test.js
@@ -28,16 +28,15 @@ contract('Decentragram', ([deployer, author, tipper]) => {
 
   describe('images', async () => {
     let result, imageCount
-    const hash = 'QmV8cfu6n4NT5xRr2AHdKxFMTZEJrA44qgrBCr739BN9Wb'
+    const hash = 'abc123'
 
-    before(async () => {
+    before(async() => {
       result = await decentragram.uploadImage(hash, 'Image description', { from: author })
       imageCount = await decentragram.imageCount()
     })
 
-    //check event
     it('creates images', async () => {
-      // SUCESS
+      // SUCCESS
       assert.equal(imageCount, 1)
       const event = result.logs[0].args
       assert.equal(event.id.toNumber(), imageCount.toNumber(), 'id is correct')
@@ -46,16 +45,14 @@ contract('Decentragram', ([deployer, author, tipper]) => {
       assert.equal(event.tipAmount, '0', 'tip amount is correct')
       assert.equal(event.author, author, 'author is correct')
 
-
-      // FAILURE: Image must have hash
+      // FAILURE : Image must have hash
       await decentragram.uploadImage('', 'Image description', { from: author }).should.be.rejected;
 
-      // FAILURE: Image must have description
-      await decentragram.uploadImage('Image hash', '', { from: author }).should.be.rejected;
+      // FAILURE : Image must have description
+      await decentragram.uploadImage('Image Hash', '', { from: author }).should.be.rejected;
     })
 
-    //check from Struct
-    it('lists images', async () => {
+    it('list images', async () => {
       const image = await decentragram.images(imageCount)
       assert.equal(image.id.toNumber(), imageCount.toNumber(), 'id is correct')
       assert.equal(image.hash, hash, 'Hash is correct')
@@ -64,7 +61,7 @@ contract('Decentragram', ([deployer, author, tipper]) => {
       assert.equal(image.author, author, 'author is correct')
     })
 
-    it('allows users to tip images', async () => {
+    it('allows users to tip images', async () =>{
       // Track the author balance before purchase
       let oldAuthorBalance
       oldAuthorBalance = await web3.eth.getBalance(author)
@@ -80,7 +77,7 @@ contract('Decentragram', ([deployer, author, tipper]) => {
       assert.equal(event.tipAmount, '1000000000000000000', 'tip amount is correct')
       assert.equal(event.author, author, 'author is correct')
 
-      // Check that author received funds
+      // Check the author receives fund
       let newAuthorBalance
       newAuthorBalance = await web3.eth.getBalance(author)
       newAuthorBalance = new web3.utils.BN(newAuthorBalance)
@@ -93,8 +90,8 @@ contract('Decentragram', ([deployer, author, tipper]) => {
 
       assert.equal(newAuthorBalance.toString(), expectedBalance.toString())
 
-      // FAILURE: Tries to tip a image that does not exist
-      await decentragram.tipImageOwner(99, { from: tipper, value: web3.utils.toWei('1', 'Ether')}).should.be.rejected;
+      // FAILURE: Tries to tip a image that doesn't exist
+      await decentragram.tipImageOwner(99, { from: tipper, value: web3.utils.toWei('1', 'Ether') }).should.be.rejected;
     })
   })
 })
